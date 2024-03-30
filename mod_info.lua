3350a03d68bb3fd4e679d169568bd50d33583226
Vivien <A-xis@users.noreply.github.com>
2024-03-30 10:59:22 +0100

    fix: Seraph ACU can't build T3 shield (fix#7)

--- mod_info.lua
+++ mod_info.lua
@@ -1,22 +1,23 @@
-name = "Jay Infrastructure Pack"
-uid = "c413b319-ax1s-infra-pack-release109"
-version = 109
-copyright = "Jay, PJ, Uveso, Packer, PatientBeard"
-description = "AI Upgradeable T2 & T3 E/M-Storage\nT2+ & T3+ PGens\nT2 & T3 & Exp HC Plant\nT3+ MEX\nT4 Shields\nReclaim Turret (by Packer/PatientBeard)"
-author = "Jay, PJ, big thank you to Uveso and whole modding community, Packer/PatientBeard for Reclaim Turret"
-icon = "/mods/Jay_Infrastructure_Pack/mod_icon.png"
-github = "https://github.com/A-xis/Jay_Infrastructure_Pack"
-selectable = true
-enabled = true
-exclusive = false
-ui_only = false
-requires = { }
+name          = "Jay Infrastructure Pack"
+uid           = "c413b319-ax1s-infra-pack-release109"
+version       = 109
+copyright     = "Jay, PJ, Uveso, Packer, PatientBeard"
+description   = "AI Upgradeable T2 & T3 E/M-Storage\nT2+ & T3+ PGens\nT2 & T3 & Exp HC Plant\nT3+ MEX\nT4 Shields\nReclaim Turret (by Packer/PatientBeard)"
+author        = "Jay, PJ, big thank you to Uveso and whole modding community, Packer/PatientBeard for Reclaim Turret"
+icon          = "/mods/Jay_Infrastructure_Pack/mod_icon.png"
+url           = "https://github.com/A-xis/Jay_Infrastructure_Pack"
+github        = "https://github.com/A-xis/Jay_Infrastructure_Pack"
+selectable    = true
+enabled       = true
+exclusive     = false
+ui_only       = false
+requires      = { }
 requiresNames = { }
-conflicts = {
+conflicts     = {
 		"c413b319-ax1s-42d5-b62b-3115ce012ccf" -- Jay Infra Pack -- 108
 	}
-before = {"9e8ea941-c306-4751-b367-a00000018000"}
-after = {"9e8ea941-c306-aaaf-b367-u00000019000",
+before        = {"9e8ea941-c306-4751-b367-a00000018000"}
+after         = {"9e8ea941-c306-aaaf-b367-u00000019000",
 		"7dd8a7469-65e8-4f45-91ab-bf4fbfe95725",
 		"9e8ee941-c406-4751-b367-e10000019000",
 		"16247194-E3B4-11E9-00390-4C1EE114E1EA",
@@ -30,7 +31,7 @@ after = {"9e8ea941-c306-aaaf-b367-u00000019000",
 		"b117ef40-2b6b-4596-96c5-0e8887ac0807",
 		"ad271f12-c9e9-4222-870b-fe68b842f52b"}
 
-history = [[
+history       = [[
 
 ### Version 109 - 24/03/2024
  - Feature: Allow T2 and T3 Storages to be built by engi and commander (https://github.com/A-xis/Jay_Infrastructure_Pack/issues/1)
